2db36fa3378f46edc0a33eab163c69cb15ad0186
jordi casanueva <jordicasanueva@gmail.com>
2020-05-20 11:36:42 -0400

    subir remove breadcrumbs php

---
+++ woocommerce/includes/wc-remove-breadcrumbs.php
@@ -0,0 +1,7 @@
+/**
+ * Remove the breadcrumbs 
+ */
+add_action( 'init', 'woo_remove_wc_breadcrumbs' );
+function woo_remove_wc_breadcrumbs() {
+    remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0 );
+}
