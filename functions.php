ca2d6a11bbf75e67d7ad2ede3722ca1a2c9a18c3
jordi casanueva <jordicasanueva@gmail.com>
2020-05-26 12:26:29 -0400

    cambio ajustes grilla woocommerce

--- functions.php
+++ functions.php
@@ -59,19 +59,7 @@ add_action('init', 'modify_jquery');
 
  ?>
 <?php function mytheme_add_woocommerce_support() {
-    add_theme_support( 'woocommerce', array(
-        'thumbnail_image_width' => 150,
-        'single_image_width'    => 300,
-
-        'product_grid'          => array(
-            'default_rows'    => 3,
-            'min_rows'        => 2,
-            'max_rows'        => 8,
-            'default_columns' => 4,
-            'min_columns'     => 2,
-            'max_columns'     => 5,
-        ),
-    ) );
+    add_theme_support( 'woocommerce' );
 }
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' ); ?>
 <?php
