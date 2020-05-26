23b3a6d7215758f28aeaf0c7575e52be4ea7de87
jordi casanueva <jordicasanueva@gmail.com>
2020-05-26 12:38:05 -0400

    columns de productos grilla de 3

--- functions.php
+++ functions.php
@@ -59,7 +59,19 @@ add_action('init', 'modify_jquery');
 
  ?>
 <?php function mytheme_add_woocommerce_support() {
-    add_theme_support( 'woocommerce' );
+    add_theme_support( 'woocommerce', array(
+        'thumbnail_image_width' => 150,
+        'single_image_width'    => 300,
+
+        'product_grid'          => array(
+            'default_rows'    => 3,
+            'min_rows'        => 2,
+            'max_rows'        => 8,
+            'default_columns' => 4,
+            'min_columns'     => 3,
+            'max_columns'     => 3,
+        ),
+    ) );
 }
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' ); ?>
 <?php
