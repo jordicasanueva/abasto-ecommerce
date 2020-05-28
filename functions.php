3331ca7cfae1cfc8055005a2cefe311d8210404c
jordi casanueva <jordicasanueva@gmail.com>
2020-05-28 10:13:45 -0400

    remove related products

--- functions.php
+++ functions.php
@@ -110,3 +110,11 @@ function jk_masonry() {
 }
 
 ?>
+<?php
+
+/**
+ * Remove related products output
+ */
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
+
+?>
